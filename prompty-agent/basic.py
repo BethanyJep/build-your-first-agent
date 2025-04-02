7e0e5b30ef1231727bff976c80aaee5f9545c4f5
Pikachú <bethanycheum@microsoft.com>
2025-04-02 08:24:14 +0000

    working - categorize prompty

--- prompty-agent/basic.py
+++ prompty-agent/basic.py
@@ -1,10 +1,15 @@
 import json
 import prompty
+from pathlib import Path
 # to use the azure invoker make 
 # sure to install prompty like this:
 # pip install prompty[azure]
 import prompty.azure
 from prompty.tracer import trace, Tracer, console_tracer, PromptyTracer
+from dotenv import load_dotenv
+
+# load environment variables from .env file
+load_dotenv()
 
 # add console and json tracer:
 # this only has to be done once
@@ -19,6 +24,7 @@ Tracer.add("PromptyTracer", json_tracer.tracer)
 @trace
 def run(    
       title: any,
+      tags: any,
       description: any
 ) -> str:
 
@@ -27,6 +33,7 @@ def run(
     "basic.prompty", 
     inputs={
       "title": title,
+      "tags": tags,
       "description": description
     }
   )
@@ -34,11 +41,11 @@ def run(
   return result
 
 if __name__ == "__main__":
-   json_input = '''{
-  "title": "Including Image in System Message",
-  "description": "An error arises in the flow, coming up starting from the \"complete\" block. It seems like it is caused by placing a static image in the system prompt, since removing it causes the issue to go away. Please let me know if I can provide additional context."
-}'''
-   args = json.loads(json_input)
+   base = Path(__file__).parent
 
-   result = run(**args)
+   title = ("Including Image in System Message")
+   description = ("An error arises in the flow, coming up starting from the complete block. It seems like it is caused by placing a static image in the system prompt, since removing it causes the issue to go away. Please let me know if I can provide additional context.")
+   tags = json.loads(Path(base, "tags.json").read_text())
+
+   result = run(title, tags, description)
    print(result)
